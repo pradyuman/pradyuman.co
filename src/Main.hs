3a83c16d43e2fa46706b9f72e61604f1336b8c88
Pradyuman Vig <pradyumanvig@outlook.com>
2018-12-22 18:38:22 -0800

    add styles

--- src/Main.hs
+++ src/Main.hs
@@ -5,9 +5,15 @@ import Hakyll
 
 main :: IO ()
 main = hakyll $ do
+
   match "images/*" $ do
     route idRoute
     compile copyFileCompiler
+
+
+  match "css/*" $ do
+      route   idRoute
+      compile compressCssCompiler
 
   match "templates/*" $ compile templateBodyCompiler
 
